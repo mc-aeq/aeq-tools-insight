9c6c3589e03abc44e2c7a851d7305f0379bf9a97
Alex Yocom-Piatt <ay-p@decred.org>
2016-09-19 15:30:31 -0500

    Add new sorting function to txs lists.  Most recent first. (#28)
    Fixes #18

--- public/src/js/controllers/transactions.js
+++ public/src/js/controllers/transactions.js
@@ -80,6 +80,18 @@ function($scope, $rootScope, $routeParams, $location, Global, Transaction, Trans
   };
 
   var _paginate = function(data) {
+    // Add new sorting by time. Newest first.
+    newdata = data.txs.sort(function(a, b) {
+      if (a.time < b.time) {
+        return 1;
+      }
+      if (a.time > b.time) {
+        return -1;
+      }
+      return 0;
+    });
+
+    data.txs = newdata;
     $scope.loading = false;
 
     pagesTotal = data.pagesTotal;
