79dd8da7524c1d99cdc6e5a9b180be3f301d3d3c
f0xs4k3 <>
2018-04-24 16:02:47 +0200

    AEQ changes

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'DCR';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'AEQ';
 
 angular.module('insight',[
   'ngclipboard',
--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -20,7 +20,7 @@ angular.module('insight.currency').controller('CurrencyController',
 
         if (this.symbol === 'USD') {
           response = _roundFloat((value * this.factor), 2);
-        } else if (this.symbol === 'mDCR') {
+        } else if (this.symbol === 'mAEQ') {
           this.factor = 1000;
           response = _roundFloat((value * this.factor), 5);
         } else if (this.symbol === 'atoms') {
@@ -45,7 +45,7 @@ angular.module('insight.currency').controller('CurrencyController',
         Currency.get({}, function(res) {
           $rootScope.currency.factor = $rootScope.currency.bitstamp = res.data.bitstamp;
         });
-      } else if (currency === 'mDCR') {
+      } else if (currency === 'mAEQ') {
         $rootScope.currency.factor = 1000;
       } else if (currency === 'atoms') {
         $rootScope.currency.factor = 100000000;
